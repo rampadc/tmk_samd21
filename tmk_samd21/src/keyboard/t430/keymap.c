fab782d0a97d438de5158e1d53091884d710e48b
Cong Nguyen <rampadc@users.noreply.github.com>
2018-01-19 18:54:56 +1100

    Adding base files for T430 example

---
+++ tmk_samd21/src/keyboard/t430/keymap.c
@@ -0,0 +1,14 @@
+/*
+ * keymap.c
+ *
+ * Created: 2/01/2018 4:15:02 PM
+ *  Author: cong
+ */ 
+
+ #include <asf.h>
+ #include "keycode.h"
+ #include "action.h"
+
+ const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	{{KC_A}}
+ };
